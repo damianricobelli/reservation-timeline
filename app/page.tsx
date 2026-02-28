2a15d706cd6c700e37b484349aed2e34e962506c
damianricobelli <dricobelli@gmail.com>
2026-02-27 23:56:20 -0300

    chore: remove console log

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { createSeedData } from "@/data/create-seed-data";
 import Link from "next/link";
 
 export default function Page() {
-  console.log(createSeedData(10));
   return (
     <main className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-2xl font-bold">Reservation Timeline</h1>
